89d349c7f7b1d38a1d9b54f01b0be5453506776f
Rakib Hasan <92901083+rakibhasan3948@users.noreply.github.com>
2023-09-15 07:30:49 +0600

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,47 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "1. Which company owns the android?",
+            "2. Which one is not the programming language?",
+            "3. Where you are watching this video?",
+            "4. Which company owns the Apple?",
+            "5. Android is -",
+            "6. Under which of the following Android is licensed?",
+            "7. For which of the following Android is mainly developed?",
+            "8. Which of the following is the first mobile phone released that ran the Android OS?",
+            "9. Which of the following virtual machine is used by the Android operating system?",
+            "10. Android is based on which of the following language?",
+            "11. What is your name?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"an operating system","a web browser","a web server","none of the above"},
+            {"OSS","Sourceforge","Apache/MIT","None of the above"},
+            {"Servers","Desktops","Laptops","Mobile devices"},
+            {"HTC Hero","Google gPhone","T - Mobile G1","None of the above"},
+            {"JVM","Dalvik virtual machine","Simple virtual machine","None of the above"},
+            {"Java","C++","C","None of the above"},
+            {"Rakib","Sakib","Hasan","Shovon"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "an operating system",
+            "Apache/MIT",
+            "Mobile devices",
+            "T - Mobile G1",
+            "Dalvik virtual machine",
+            "Java",
+            "Rakib"
+    };
+
+}
